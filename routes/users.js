269dc8ab824eaf895f9473b122842fba1f6ab5e6
John-O <johnoladosu@outlook.com>
2025-12-03 10:00:36 +0000

    Fix redirect path in redirectLogin function to ensure correct login redirection

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ const saltRounds = 10;
 
 const redirectLogin = (req, res, next) => {
   if (!req.session.userId) {
-    res.redirect('../users/login');
+    res.redirect('/users/login');
   } else {
     next();
   }
